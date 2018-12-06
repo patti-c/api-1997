d6023c1263697b888ad5490d3dc84b8923e5624c
patti-c <patricia.carter@flatironschool.com>
2018-12-06 09:52:05 -0500

    password validation basic

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: true
+  validates :password, presence: true
 
   # DATABASE RELATIONSHIPS
 
