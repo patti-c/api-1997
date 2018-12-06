5825238e39befbda46822c063d9c8f31252f4ab2
patti-c <patricia.carter@flatironschool.com>
2018-12-06 11:40:17 -0500

    further environment configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://localhost:3000/cable'
+  config.action_cable.url = 'wss://api-1997.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ /http:\/\/localhost:*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
